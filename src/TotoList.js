4e81943aab2bdff2bbebfac3a512624b097492d2
rick <dxq7541@gmail.com>
2020-08-15 21:51:19 -0400

    temp

---
+++ src/TotoList.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+function TotoList() {
+  return (
+    <Fragment>
+      <div><input /><button>提交</button></div>
+      <ul>
+        <li>学习</li>
+        <li>开始</li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default TotoList;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TotoList from './TotoList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TotoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
