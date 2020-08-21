c01759c96ecf5f21f71f842702af06e3791ceb7d
rick <dxq7541@gmail.com>
2020-08-20 21:21:40 -0400

    ajax and json-server

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Addition, HeaderWrapper, Logo, Nav, NavItem,
   NavSearch, Button, SearchWrapper, SearchInfo,
@@ -8,75 +8,83 @@ import {
 import { CSSTransition } from "react-transition-group";
 import { connect } from "react-redux";
 import * as store from './store'
+import { actionCreators } from "./store";
 
-const getListArea = (show) => {
-  if (show) {
+class Header extends Component {
+  getListArea = () => {
+    const {focused, list} = this.props
+    if (focused) {
+      return (
+        <SearchInfo>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>换一批</SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            {
+              list.map((item) => {
+                return <SearchInfoItem key={item}>{item}</SearchInfoItem>
+              })
+            }
+          </SearchInfoList>
+        </SearchInfo>
+      )
+    } else {
+      return null
+    }
+  }
+
+  render() {
+    const {focused, onBlur, onFocus} = this.props
     return (
-      <SearchInfo>
-        <SearchInfoTitle>
-          热门搜索
-          <SearchInfoSwitch>换一批</SearchInfoSwitch>
-        </SearchInfoTitle>
-        <SearchInfoList>
-          <SearchInfoItem>教育</SearchInfoItem>
-          <SearchInfoItem>大学</SearchInfoItem>
-          <SearchInfoItem>热点</SearchInfoItem>
-          <SearchInfoItem>开门红</SearchInfoItem>
-          <SearchInfoItem>过年好</SearchInfoItem>
-        </SearchInfoList>
-      </SearchInfo>
+      <HeaderWrapper>
+        <Logo/>
+        <Nav>
+          <NavItem className='left active'>Home</NavItem>
+          <NavItem className='left'>Download App</NavItem>
+          <NavItem className='right'>Login</NavItem>
+          <NavItem className='right'>
+            <span className="iconfont">&#xe636;</span>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              in={focused}
+              timeout={200}
+              classNames='slide'
+            >
+              <NavSearch
+                className={focused ? 'focused' : 'unfocused'}
+                onFocus={onFocus}
+                onBlur={onBlur}
+              />
+            </CSSTransition>
+            <span className={focused ? 'focused iconfont' : 'unfocused iconfont'}>&#xe610;</span>
+            {this.getListArea(focused)}
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className='writing'>
+            <span className="iconfont">&#xe708;</span>
+            Write Article
+          </Button>
+          <Button className='reg'>Register</Button>
+        </Addition>
+      </HeaderWrapper>
     )
-  } else {
-    return null
   }
 }
-const Header = (props) => {
-  return (
-    <HeaderWrapper>
-      <Logo/>
-      <Nav>
-        <NavItem className='left active'>Home</NavItem>
-        <NavItem className='left'>Download App</NavItem>
-        <NavItem className='right'>Login</NavItem>
-        <NavItem className='right'>
-          <span className="iconfont">&#xe636;</span>
-        </NavItem>
-        <SearchWrapper>
-          <CSSTransition
-            in={props.focused}
-            timeout={200}
-            classNames='slide'
-          >
-            <NavSearch
-              className={props.focused ? 'focused' : 'unfocused'}
-              onFocus={props.onFocus}
-              onBlur={props.onBlur}
-            />
-          </CSSTransition>
-          <span className={props.focused ? 'focused iconfont' : 'unfocused iconfont'}>&#xe610;</span>
-          {getListArea(props.focused)}
-        </SearchWrapper>
-      </Nav>
-      <Addition>
-        <Button className='writing'>
-          <span className="iconfont">&#xe708;</span>
-          Write Article
-        </Button>
-        <Button className='reg'>Register</Button>
-      </Addition>
-    </HeaderWrapper>
-  )
-};
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.getIn(['header', 'focused'])
+    focused: state.getIn(['header', 'focused']),
+    list: state.getIn(['header', 'list'])
   }
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onFocus: () => {
+      dispatch(actionCreators.getList())
       dispatch(store.actionCreators.searchFocus())
     },
     onBlur: () => {
--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -1,4 +1,6 @@
 import * as constants from './constants'
+import axios from 'axios'
+import { fromJS } from 'immutable'
 
 export const searchFocus = () => ({
   type: constants.SEARCH_FOCUS
@@ -7,3 +9,25 @@ export const searchFocus = () => ({
 export const searchBlur = () => ({
   type: constants.SEARCH_BLUR
 })
+
+const changeList = (data) => {
+  return {
+    type: constants.CHANGE_LIST,
+    data: fromJS(data)
+  }
+}
+
+export const getList = () => {
+  return (dispatch) => {
+    let onSuccess = (res) => {
+      let data = res.data
+      dispatch(changeList(data))
+    }
+    let onError = (res) => {
+      console.warn('no data')
+    }
+    axios.get('http://localhost:3001/list')
+    // axios.get('/api/list.json')
+      .then(onSuccess, onError)
+  }
+}
